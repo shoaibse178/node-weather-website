875d08901107e7d62174a6c75c2c77217320385f
shoaibse178 <shoaibse178@gmail.com>
2020-12-27 09:45:14 +0500

    forecast new features added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude,longitude, callback) =>{
          } else if(body.message) {
              callback('unable to result for this location. try for another',undefined)
          } else{
-             callback(undefined, 'There is temp is ' + body.main.temp + ' and the wind speed is ' + body.wind.speed)
+             console.log(body)
+             callback(undefined, 'Weather main condition is '+ body.weather[0].main +'.'+' There is min tempreture is ' + body.main.temp_min +' and high tempreture '+body.main.temp_max +'.'+' Pressure is ' + body.main.pressure +' and the wind speed is ' + body.wind.speed+'.')
          }
     })
 }
